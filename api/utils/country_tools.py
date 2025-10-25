c06de815543d3af3c36b4c6842e134b1becd44e8
Tomilola <83653154+Bluel0la@users.noreply.github.com>
2025-10-25 06:06:27 +0100

    fixed something

--- api/utils/country_tools.py
+++ api/utils/country_tools.py
@@ -91,12 +91,15 @@ def fetch_countries_data(countries_url: str, exchange_base_url: str):
         print(f"[Country Fetch Error]: {e}")
         return []
 
+AWS_ACCESS_KEY_ID="0751eafbf6934156b3ba86fdef0a7d8f"
+AWS_SECRET_ACCESS_KEY="d3b7c7f49aa71207ae977308d07a005b8f2cd35025208e823118e7d6cbd81d7a"
+
 s3 = boto3.client(
     "s3",
     region_name="us-east-1",
     endpoint_url="https://objstorage.leapcell.io",
-    aws_access_key_id=os.getenv("AWS_ACCESS_KEY_ID"),
-    aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY"),
+    aws_access_key_id="AWS_ACCESS_KEY_ID",
+    aws_secret_access_key=AWS_SECRET_ACCESS_KEY
 )
 
 BUCKET_NAME = "os-wsp1980603830540251137-vs3x-yv5n-h4cxpsz2"
