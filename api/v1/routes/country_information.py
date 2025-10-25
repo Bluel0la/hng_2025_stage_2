7385e25c4bb190ada6258eda2d6e2d37acb865ac
Tomilola <83653154+Bluel0la@users.noreply.github.com>
2025-10-25 09:21:15 +0100

    file

--- api/v1/routes/country_information.py
+++ api/v1/routes/country_information.py
@@ -70,7 +70,7 @@ def get_summary_image():
     """
     try:
         s3.head_object(Bucket=BUCKET_NAME, Key=SUMMARY_KEY)
-        file_url = f"{PUBLIC_BASE_URL}/cache/summary.png"
+        file_url = f"https://1xg7ah.leapcellobj.com/os-wsp1980603830540251137-vs3x-yv5n-h4cxpsz2/cache/summary.png"
         return RedirectResponse(url=file_url)
     except s3.exceptions.ClientError:
         return JSONResponse(
